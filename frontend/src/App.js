730ca0961f41f3bf7fdcb24cef4f213fd44d28e7
orzohar <or.zohar@campus.technion.ac.il>
2024-10-31 23:16:11 +0200

    fit table to screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState } from 'react';
 import TopAppBar from './components/TopAppBar';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MainContent from './components/MainContent';
 
