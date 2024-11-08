a08e79e9a8d2f8df247b588c753c8c9c4ab57fe8
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 21:20:47 +0200

    refine code ResultsPage.js

--- frontend/src/components/ResultsPage.js
+++ frontend/src/components/ResultsPage.js
@@ -1,18 +1,20 @@
 // src/ResultsPage.js
-import React, {useState, useEffect} from 'react';
-import {Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
 import axios from 'axios';
-import LeagueDropdown from './LeagueDropdown';
-import TeamDropdown from './TeamDropdown';
 import GameTable from './GameTable';
 import SelectLeagueAndTeam from "./LeagueTeamSelect";
 import config from "../config";
+
+const getGamesUrl = (leagueId, teamId) => `${config.API_GET_ALL_GAMES}/${leagueId}/${teamId}`;
 
 export default function ResultsPage() {
     const [selectedLeague, setSelectedLeague] = useState('');
     const [selectedTeam, setSelectedTeam] = useState('');
     const [games, setGames] = useState([]);
     const [showGames, setShowGames] = useState(false);
+    const [error, setError] = useState(null);
+
 
     const handleLeagueSelect = (leagueId) => {
         setSelectedLeague(leagueId);
@@ -29,10 +31,12 @@ export default function ResultsPage() {
         if (selectedTeam) {
             async function fetchGames() {
                 try {
-                    const response = await axios.get(`${config.API_GET_ALL_GAMES}/${selectedLeague}/${selectedTeam}`);
+                    const response = await axios.get(getGamesUrl(selectedLeague, selectedTeam));
                     setGames(response.data);
+                    setError(null); // Clear error if successful
                 } catch (error) {
                     console.error('Error fetching games:', error);
+                    setError('Failed to fetch game data. Please try again later.');
                 }
             }
 
@@ -43,13 +47,14 @@ export default function ResultsPage() {
     }, [selectedTeam]);
 
     return (
-        <Box sx={{p: 3}}>
+        <Box sx={{padding: 3}}>
             <SelectLeagueAndTeam
                 handleLeagueSelect={handleLeagueSelect}
                 handleTeamSelect={handleTeamSelect}
                 selectedLeague={selectedLeague}/>
+            {error && <Typography color="error">{error}</Typography>}
             {showGames && (
-                <Box sx={{mt: 4}}>
+                <Box sx={{marginTop: 4}}>
                     <Typography variant="h6" gutterBottom>
                         Results
                     </Typography>
