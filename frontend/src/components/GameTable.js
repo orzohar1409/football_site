12f8899b3d824488e0e17f5f321e366acc425924
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 17:05:32 +0200

    fix configuration of table cell in game table page

--- frontend/src/components/GameTable.js
+++ frontend/src/components/GameTable.js
@@ -1,31 +1,37 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box } from "@mui/material";
 import React from "react";
 
+const cellStyles = {
+    fontWeight: 'bold',
+    fontSize: { xs: '0.55rem', sm: '0.8rem' },
+    padding: { xs: 0.5, sm: 1 }
+};
+
 export default function GameTable({ games }) {
     return (
         <TableContainer component={Paper} sx={{ maxHeight: 250, overflowY: 'auto', width: '100%' }}>
             <Table stickyHeader>
                 <TableHead>
                     <TableRow>
-                        <TableCell sx={{ fontWeight: 'bold', fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>Date</TableCell>
-                        <TableCell sx={{ fontWeight: 'bold', fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>Home Team</TableCell>
-                        <TableCell sx={{ fontWeight: 'bold', fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>Away Team</TableCell>
-                        <TableCell sx={{ fontWeight: 'bold', fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>Result</TableCell>
+                        <TableCell sx={cellStyles}>Date</TableCell>
+                        <TableCell sx={cellStyles}>Home Team</TableCell>
+                        <TableCell sx={cellStyles}>Away Team</TableCell>
+                        <TableCell sx={cellStyles}>Result</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {games.map((game, index) => (
                         <TableRow key={index} sx={{ height: { xs: 30, sm: 50 } }}> {/* Smaller row height on mobile */}
-                            <TableCell sx={{ fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>
+                            <TableCell sx={cellStyles}>
                                 {new Date(game.date).toLocaleDateString()}
                             </TableCell>
-                            <TableCell sx={{ fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>
+                            <TableCell sx={cellStyles}>
                                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                                     <img
                                         src={game.home_team.logo}
                                         alt={`${game.home_team.name} logo`}
                                         style={{
-                                            width: '0.75rem',  // Even smaller for mobile
+                                            width: '0.75rem',
                                             height: '0.75rem',
                                             marginRight: 2
                                         }}
@@ -33,13 +39,13 @@ export default function GameTable({ games }) {
                                     {game.home_team.name}
                                 </Box>
                             </TableCell>
-                            <TableCell sx={{ fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>
+                            <TableCell sx={cellStyles}>
                                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                                     <img
                                         src={game.away_team.logo}
                                         alt={`${game.away_team.name} logo`}
                                         style={{
-                                            width: '0.75rem',  // Even smaller for mobile
+                                            width: '0.75rem',
                                             height: '0.75rem',
                                             marginRight: 2
                                         }}
@@ -47,7 +53,7 @@ export default function GameTable({ games }) {
                                     {game.away_team.name}
                                 </Box>
                             </TableCell>
-                            <TableCell sx={{ fontSize: { xs: '0.55rem', sm: '0.8rem' }, padding: { xs: 0.5, sm: 1 } }}>{game.result}</TableCell>
+                            <TableCell sx={cellStyles}>{game.result}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
