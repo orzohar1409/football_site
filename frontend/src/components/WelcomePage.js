0e2cbc2e01fea16e88537be380c4d1575046e2d8
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 21:54:04 +0200

    refine code WelcomePage.js

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -2,7 +2,7 @@ import {Box, IconButton, Link, Slide, Typography} from "@mui/material";
 import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
-export default function WelcomePage({onStartClick}) {
+export default function WelcomePage() {
     return (<>
             <Slide direction="up" in={true} mountOnEnter unmountOnExit timeout={{
                 enter: 1000,
@@ -19,13 +19,12 @@ export default function WelcomePage({onStartClick}) {
                         textAlign: 'center',
                     }}
                 >
-                    {/* Replace 'logo.png' with the path to your logo image */}
-
                     <Typography variant="h4" component="h1" gutterBottom>
                         Welcome to Or Zohar's Football Site!
                     </Typography>
                     <Box
                         component="img"
+                        alt="Or Zohar's Football Site Logo"
                         sx={{
                             maxHeight: {xs: 200, md: 200},
                             maxWidth: {xs: 200, md: 250},
@@ -45,7 +44,7 @@ export default function WelcomePage({onStartClick}) {
                 bottom: 0,
             }}
             >
-                <Typography variant="body2" color="textSecondary">
+                <Typography variant="body2" color="text.secondary">
                     Check my project on{' '}
                     <Link
                         href="https://github.com/orzohar1409/football_site"
