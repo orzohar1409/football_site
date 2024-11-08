9a7214e0fb036760a8bd8e209292d054a648dc52
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 19:31:37 +0200

    add logo to top app bar

--- frontend/src/components/TopAppBar.js
+++ frontend/src/components/TopAppBar.js
@@ -1,21 +1,28 @@
 // src/components/TopAppBar.js
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import {AppBar, Toolbar, Typography, IconButton, Box} from "@mui/material";
 import DehazeOutlinedIcon from '@mui/icons-material/DehazeOutlined';
 import CloseIcon from '@mui/icons-material/Close';
-export default function TopAppBar({ onMenuClick, isDrawerOpen }) {
+import {Image} from "@mui/icons-material";
+
+export default function TopAppBar({onMenuClick, isDrawerOpen}) {
     return (
-        <AppBar position="fixed" sx={{ height: 64,
+        <AppBar position="fixed" sx={{
+            height: 64,
             zIndex: (theme) => theme.zIndex.drawer + 1,
             backgroundColor: 'rgb(241,24,24)', // or another color
-            backgroundPosition: 'center',}}>
+            backgroundPosition: 'center',
+        }}>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu" onClick={onMenuClick}>
-                    {isDrawerOpen ? <CloseIcon /> : <DehazeOutlinedIcon />}
+                    {isDrawerOpen ? <CloseIcon/> : <DehazeOutlinedIcon/>}
                 </IconButton>
-                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                <Typography variant="h6" sx={{flexGrow: 1}}>
                     Ballon d'Or Zohar
                 </Typography>
+                <Box component="img" src="../../logo.png" sx={{height:64,
+                    justifyContent: "flex-end",
+                }}/>
             </Toolbar>
         </AppBar>
     );
