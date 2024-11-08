248e94f0e4beae9bf310cc3d0019a6771c2385a9
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 21:32:32 +0200

    refine code LeagueTeamSelect.js

--- frontend/src/components/LeagueTeamSelect.js
+++ frontend/src/components/LeagueTeamSelect.js
@@ -1,32 +1,32 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import {Box, Slide, Typography} from '@mui/material';
 import LeagueDropdown from './LeagueDropdown';
 import TeamDropdown from './TeamDropdown';
 
-export default function SelectLeagueAndTeam({ handleLeagueSelect, handleTeamSelect, selectedLeague }) {
-    const [isVisible, setIsVisible] = useState(true);
-    return (
+const containerStyles = {
+    overflow: 'hidden',
+    transform: 'translateY(0)',
+    opacity: 1,
+    transition: 'transform 2s ease, opacity 1s ease',
+    mb: 1,
+    height: '120px',
+};
 
-        <Box
-            sx={{
-                overflow: 'hidden',
-                transform: isVisible ? 'translateY(0)' : 'translateY(-100%)',
-                opacity: isVisible ? 1 : 0,
-                transition: 'transform 2s ease, opacity 1s ease',
-                backdropFilter: 'blur(8px)', // Blur effect
-                borderRadius: '8px', // Optional: rounded corners
-                mb: 2,
-                height: '120px'  // Fixed height for the dropdown area
-            }}
-        >
-
+const dropdownContainerStyles = {
+    display: 'flex',
+    gap: 2,
+    alignItems: 'center',
+};
+export default function SelectLeagueAndTeam({handleLeagueSelect, handleTeamSelect, selectedLeague}) {
+    return (<Slide direction="down" in={true} mountOnEnter unmountOnExit>
+        <Box sx={containerStyles}>
             <Typography variant="h5" gutterBottom>
                 Select League and Team
             </Typography>
-            <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-                <LeagueDropdown onLeagueSelect={handleLeagueSelect} />
-                <TeamDropdown selectedLeague={selectedLeague} onTeamSelect={handleTeamSelect} />
+            <Box sx={dropdownContainerStyles}>
+                <LeagueDropdown onLeagueSelect={handleLeagueSelect}/>
+                <TeamDropdown selectedLeague={selectedLeague} onTeamSelect={handleTeamSelect}/>
             </Box>
-        </Box>
+        </Box></Slide>
     );
 }
