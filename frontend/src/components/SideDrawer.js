ecd23f962638fb065f257fb86d2c964192fc6143
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 21:13:03 +0200

    refine SideDrawer.js code

--- frontend/src/components/SideDrawer.js
+++ frontend/src/components/SideDrawer.js
@@ -1,46 +1,49 @@
 // src/components/SideDrawer.js
 import React from 'react';
 import { Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
-import { Home, AccountCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { appPages } from '../PagesConfig'; // Import your configuration file
 
-export default function SideDrawer({ isOpen, onClick }) {
+export default function SideDrawer({ isOpen, onPageClick }) {
+    const drawerStyles = {
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+            marginTop: '64px', // Start below the App Bar
+            backgroundColor: 'rgba(73,50,54,0.7)', // Semi-transparent background
+            backgroundSize: 'cover', // Make the background cover the drawer area
+            backgroundPosition: 'center', // Center the background image
+            color: 'white', // Adjust text color if needed for contrast
+        },
+    };
+
+    const listItemStyles = {
+        '& .MuiListItem-root': {
+            color: 'white',  // Default text color for all ListItems
+                '&:hover': {
+                backgroundColor: 'rgba(0, 0, 0, 0.08)', // Hover background color
+            },
+            '&.Mui-selected': {
+                backgroundColor: 'transparent', // Remove blue highlight color on selection
+            },
+        },
+    };
+
     return (
         <Drawer
             variant="persistent"
             anchor="left"
             open={isOpen}
-            sx={{
-                flexShrink: 0,
-                '& .MuiDrawer-paper': {
-                    width: 240,
-                    boxSizing: 'border-box',
-                    marginTop: '64px', // Start below the App Bar
-                    backgroundColor: 'rgba(73,50,54,0.7)', // Semi-transparent background
-                    backgroundSize: 'cover', // Make the background cover the drawer area
-                    backgroundPosition: 'center', // Center the background image
-                    color: 'white', // Adjust text color if needed for contrast
-                },
-            }}
+            sx={drawerStyles}
         >
             <List
-                sx={{
-                    '& .MuiListItem-root': {
-                        color: 'white',  // Default text color for all ListItems
-                        '&:hover': {
-                            backgroundColor: 'rgba(0, 0, 0, 0.08)', // Hover background color
-                        },
-                        '&.Mui-selected': {
-                            backgroundColor: 'transparent', // Remove blue highlight color on selection
-                        },
-                    },
-                }}
+                sx={listItemStyles}
             >
                 {appPages.map((page, index) => (
                     <React.Fragment key={index}>
-                        <ListItem button component={Link} to={page.path} onClick={onClick}>
-                            <ListItemIcon>{page.icon()}</ListItemIcon>
+                        <ListItem button component={Link} to={page.path} onClick={onPageClick}>
+                            <ListItemIcon aria-hidden="true">{page.icon()}</ListItemIcon>
                             <ListItemText primary={page.name} />
                         </ListItem>
                         {index < appPages.length - 1 && (
