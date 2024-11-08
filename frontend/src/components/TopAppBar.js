eb28a2ea38301e03d1c5d37fcfc7325d6cac9596
orzohar <or.zohar@campus.technion.ac.il>
2024-11-08 19:57:05 +0200

    refine TopAppBar code

--- frontend/src/components/TopAppBar.js
+++ frontend/src/components/TopAppBar.js
@@ -3,26 +3,36 @@ import React from "react";
 import {AppBar, Toolbar, Typography, IconButton, Box} from "@mui/material";
 import DehazeOutlinedIcon from '@mui/icons-material/DehazeOutlined';
 import CloseIcon from '@mui/icons-material/Close';
-import {Image} from "@mui/icons-material";
+import logo from '../assets/logo.png';
 
 export default function TopAppBar({onMenuClick, isDrawerOpen}) {
+    const appBarStyles = {
+        height: 64,
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        backgroundColor: 'rgb(241,24,24)',
+    };
+
+    const logoStyles = {
+        height: 50,
+        display: "flex",
+        alignItems: "center",
+    };
+
     return (
-        <AppBar position="fixed" sx={{
-            height: 64,
-            zIndex: (theme) => theme.zIndex.drawer + 1,
-            backgroundColor: 'rgb(241,24,24)', // or another color
-            backgroundPosition: 'center',
-        }}>
+        <AppBar position="fixed" sx={appBarStyles}>
             <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu" onClick={onMenuClick}>
-                    {isDrawerOpen ? <CloseIcon/> : <DehazeOutlinedIcon/>}
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label={isDrawerOpen ? "close menu" : "open menu"}
+                    onClick={onMenuClick}
+                >
+                    {isDrawerOpen ? <CloseIcon /> : <DehazeOutlinedIcon />}
                 </IconButton>
                 <Typography variant="h6" sx={{flexGrow: 1}}>
                     Ballon d'Or Zohar
                 </Typography>
-                <Box component="img" src="../../logo.png" sx={{height:64,
-                    justifyContent: "flex-end",
-                }}/>
+                <Box component="img" src={logo} sx={logoStyles}/>
             </Toolbar>
         </AppBar>
     );
