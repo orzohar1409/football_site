6acecebac498eaaa0d9fdb69b93ea51fcf69b1b0
orzohar <or.zohar@campus.technion.ac.il>
2024-11-13 22:21:04 +0200

    fix glocal team and league use

--- frontend/src/components/GameWidget.js
+++ frontend/src/components/GameWidget.js
@@ -3,13 +3,13 @@ import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
 import sharedConfig from "../config"; // Ensure this file exports API_KEY
 
-export default function ResWidget() {
+export default function GameWidget({gameId}) {
 
     return (
         <div id="wg-api-football-game"
              data-host="v3.football.api-sports.io"
              data-key={sharedConfig.API_KEY}
-             data-id="718243"
+             data-id={gameId}
              data-theme=""
              data-refresh="15"
              data-show-errors="false"
