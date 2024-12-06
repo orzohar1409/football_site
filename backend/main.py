3bc37d6ad5ff4c1c7ffe5d6f7f9d862b4d776193
orzohar <or.zohar@campus.technion.ac.il>
2024-12-06 15:28:20 +0200

    clean prints

--- backend/main.py
+++ backend/main.py
@@ -18,36 +18,26 @@ print(PORT)
 @app.route(f'/api/{config.API_LEAGUES_ENDPOINT}')
 @cross_origin(origin=config.FRONTEND_DOMAIN)  # Allow CORS on this specific route
 def serve_get_leagues():
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    app.logger.info(f"Request from {client_ip}")
     return get_all_leagues()
 
 @app.route(f'/api/{config.API_TEAMS_ENDPOINT}/<int:league_id>')
 @cross_origin(origin=config.FRONTEND_DOMAIN)  # Allow CORS on this specific route
 def serve_get_teams(league_id):
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    app.logger.info(f"Request from {client_ip}")
     return get_all_teams_by_league(league_id)
 
 @app.route(f'/api/{config.API_GAMES_ENDPOINT}/<int:league_id>/<int:team_id>')
 @cross_origin(origin=config.FRONTEND_DOMAIN)  # Allow CORS on this specific route
 def serve_get_games(league_id, team_id):
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    app.logger.info(f"Request from {client_ip}")
     return get_all_games_by_league_and_team(league_id, team_id)
 
 @app.route(f'/api/{config.API_VENUES_ENDPOINT}/<int:league_id>')
 @cross_origin(origin=config.FRONTEND_DOMAIN)  # Allow CORS on this specific route
 def serve_get_venues(league_id):
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    app.logger.info(f"Request from {client_ip}")
     return get_venues_by_league_id(league_id)
 
 @app.route(f'/api/{config.API_SQUADS_ENDPOINT}/<int:team_id>')
 @cross_origin(origin=config.FRONTEND_DOMAIN)  # Allow CORS on this specific route
 def serve_get_squads(team_id):
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    app.logger.info(f"Request from {client_ip}")
     return get_squad_by_team_id(team_id)
 
 
